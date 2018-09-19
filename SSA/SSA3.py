4f85496501467a66ecd871cbb08def6026b912ae
Metin San <metinisan@gmail.com>
2018-09-19 16:16:47 +0200

    planck approx

--- SSA/SSA3.py
+++ SSA/SSA3.py
@@ -29,9 +29,27 @@ def planck(T,wav):
 	Plancks Law.
 	"""
 
-	return (2*h*c**2*wav**-5)*(1./(np.exp(h*c/(wav*k_erg*T))-1.))
+	return (2*h*c**2*wav**(-5))*(1./(np.exp(h*c/(wav*k_erg*T))-1.))
 
 
+def wien(T,wav):
+
+	"""
+	Wien approximation to Planck.
+
+	"""
+
+	return (2*h*c**2*wav**(-5))*np.exp(-h*c/(wav*k_erg*T))
+
+
+def rayleighjeans(T,wav):
+
+	"""
+	Rayleigh-Jeans approximation to Planck.
+
+	"""
+
+	return 2*c*k_erg*T*wav**(-4)
 
 
 
@@ -62,25 +80,57 @@ def plot_planck():
 
 
 
-	plt.yscale('log')
-	plt.xscale('log')
-	plt.title(r"Planck's Law (loglog plot) ")
+	# plt.yscale('log')
+	# plt.xscale('log')
+	plt.title(r"Planck's Law ")
 	plt.xlabel(r'$\lambda$ [$\AA$]')
 	plt.ylabel(r"$B_\lambda$")
 	plt.xlim(0,20800)
 	plt.legend()
 	plt.grid(ls ="--")
-	plt.savefig("planckloglog.pdf")
+	# plt.savefig("planck.pdf")
 	plt.show()
 
 
-plot_planck()
+# plot_planck()
 
 
+def plot_planck_approx():
+
+	"""
+	Plotting plancks law with the wien and rayleigh-Jeans approximations.
+
+	"""
+
+	wav = np.linspace(1000,20800,1000)
+	B = np.zeros(len(wav))
+	W = np.zeros(len(wav))
+	R = np.zeros(len(wav))
+	T = 10000
+
+	B[:] = planck(T, wav[:]*1e-8)
+	W[:] = wien(T, wav[:]*1e-8)
+	R[:] = rayleighjeans(T, wav[:]*1e-8)
 
 
+	plt.plot(wav,B, label = "Planck's law")
+	plt.plot(wav,W, label = "Wien's law", ls ="--")
+	plt.plot(wav,R, label = "Rayleigh-Jeans law", ls ="--")
+
+	# plt.ylim([8e13, 1e16])
+	plt.yscale("log")
+	plt.xscale("log")
+	plt.title("Wien and Rayleigh-Jeans approximations")
+	plt.xlabel(r'$\lambda$ [$\AA$]')
+	plt.ylabel(r"$B_\lambda$")
+	plt.xlim(0,20800)
+	plt.legend()
+	plt.grid(ls ="--")
+	# plt.savefig("planckapprox.pdf")
+	plt.show()
 
 
+plot_planck_approx()
 
 
 
